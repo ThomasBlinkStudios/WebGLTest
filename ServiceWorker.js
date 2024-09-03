5ac3dd8a98142759b76e88d2c06f378679aa8340
ThomasBlinkStudios <business@blinkstudios.dev>
2024-09-03 20:46:06 +0700

    brotli and other new settings

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,9 +1,9 @@
 const cacheName = "Blink Studios-Swipe Arena-0.1";
 const contentToCache = [
-    "Build/IOS.loader.js",
-    "Build/IOS.framework.js",
-    "Build/IOS.data",
-    "Build/IOS.wasm",
+    "Build/6804e234b85b91f230ae9794f52b8d67.loader.js",
+    "Build/3aff7d63567cbf4a53298012c96288e6.framework.js.unityweb",
+    "Build/8bd85e67c68d1b00ce45c217ef4d3dbf.data.unityweb",
+    "Build/28cddcd429a693fd3efddd3b7ae0b440.wasm.unityweb",
     "TemplateData/style.css"
 
 ];
